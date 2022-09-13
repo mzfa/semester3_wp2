1f5d006e3d6485bd84f5f2d2c27f8a0af695598b
mzfa <zulfikarfebri1505@gmail.com>
2022-09-13 20:50:17 +0700

    Menambahkan Controller baru

---
+++ CI/application/controllers/Contoh_1.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh_1 extends CI_Controller {
+
+	public function index()
+	{
+		echo "Biodata <br>";
+        echo "Nama Saya : Muhammad Zulfikar F A, Bapak bisa pangil saya Zulfikar, Saya program studi Sistem informasi";
+	}
+}
